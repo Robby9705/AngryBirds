50f885d45ef4c7defd92b2f393dced6f305ee071
Robby9705 <robbygroovy@gmail.com>
2021-02-04 15:49:37 +0530

    Updated Code

--- sketch.js
+++ sketch.js
@@ -37,8 +37,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-    log6 = new Log(230, 180, 80, PI/2);
-    chain = new Chain(bird.body, log6.body);
+    //log6 = new Log(230, 180, 80, PI/2);
+    slingshot = new SlingShot(bird.body, {x: 200, y: 100});
 
 }
 
@@ -62,9 +62,17 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-    log6.display();
-    chain.display();
+    //log6.display();
+    slingshot.display();
 
     bird.display();
     platform.display();
+}
+
+function mouseDragged() {
+    Matter.Body.setPosition(bird.body, {x: mouseX, y: mouseY});
+}
+
+function mouseReleased() {
+    slingshot.fly();
 }
